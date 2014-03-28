d0d7b614d220c579fc03c3cc63b3f3c5a39eb078
Yurii Kuzich <qzichs@gmail.com>
2014-03-28 12:04:24 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Index class. Runs main method.
+ *
+ * @author Kuzich Yurii <qzichs@gmail.com>
+ */
+class Index {
+
+    /**
+     * 
+     */
+    public static function main() {
+        echo "Hello World\n";
+    }
+
+}
+
+Index::main();
+?>
