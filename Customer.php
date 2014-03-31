e2b12b4b9c8160e66b36dd4d85c67243a2931e40
Yurii Kuzich <qzichs@gmail.com>
2014-03-31 13:10:13 +0300

    move method Customer.amountFor() to Rental.getCharge()

--- Customer.php
+++ Customer.php
@@ -40,42 +40,23 @@ class Customer {
         return $this->_name;
     }
 
-    private function amountFor(Rental $aRental) {
-        $result = 0;
-        switch ($aRental->getMovie()->getPriceCode()) {
-            case Movie::REGULAR:
-                $result += 2;
-                if ($aRental->getDaysRented() > 2)
-                    $result += ($aRental->getDaysRented() - 2 ) * 1.5;
-                break;
-            case Movie ::NEW_RELEASE:
-                $result += $aRental->getDaysRented() * 3;
-                break;
-            case Movie ::CHILDRENS:
-                $result += 1.5;
-                if ($aRental->getDaysRented() > 3)
-                    $result += ($aRental->getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return $result;
-    }
-
     public function statement() {
         $totalAmount = 0;
         $frequentRenterPoints = 0;
         $result = "Учет аренды для " . $this->getName() . "\n";
 
         foreach ($this->_rentals as $each) {
-            $thisAmount = $this->amountFor($each);
+            
 // добавить очки для активного арендатора
             $frequentRenterPoints ++;
 // бонус за аренду новинки на два дня
             if (($each->getMovie()->getPriceCode() == Movie :: NEW_RELEASE) &&
                     $each->getDaysRented() > 1)
                 $frequentRenterPoints ++;
+            
 //показать результаты для этой аренды
-            $result .= "\t" . $each->getMovie()->getTitle() . "\t" . $thisAmount . "\n";
-            $totalAmount += $thisAmount;
+            $result .= "\t" . $each->getMovie()->getTitle() . "\t" . $each->getCharge() . "\n";
+            $totalAmount += $each->getCharge();
         }
 //добавить нижний колонтитул
         $result .= "Сумма задолженности составляет " . $totalAmount . "\n";
